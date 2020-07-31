9f1e82772648dcfa84db8c1fa6e1e7007cf3f910
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-07-31 17:52:01 -0400

    creacion de letras en forma dinamica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,20 @@ import { Component } from '@angular/core';
 // le da funcionalidad a la clase AppComponent de forma externa
 @Component({
 
-  selector: 'app-root', 
+  selector: 'app-root',
   // selector q esta en HTML y cuando se llame, se va a renderizar todo lo q esta en app-root
-  templateUrl: './app.component.html', 
+  templateUrl: './app.component.html',
   // cual es el archivo HTML 
-styleUrls: ['./app.component.css'] 
-// agregar estilos personalizados
+  styleUrls: ['./app.component.css']
+  // agregar estilos personalizados
 })
 
 export class AppComponent {
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 
   constructor() {
     console.log("Se acaba de crear el App Component");
